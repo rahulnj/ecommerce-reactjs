b6e7e3a304dfe96de9f08203faaf51842be12f83
Rahul <rahuljayaraman8@gmail.com>
2022-04-09 10:06:59 +0530

    added the routing and navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
-import { Home } from './screens'
+import { Home, Navigation, Signin } from './screens'
 const App = () => {
+
+  const Shop = () => {
+    return <h1>I am the shop page</h1>;
+  };
+
   return (
     <Routes>
-      <Route path='/' index element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<Signin />} />
+      </Route>
     </Routes>
   );
 }
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,1 +1,3 @@
 export { default as Home } from './Home/Home';
+export { default as Navigation } from './Navigation/Navigation';
+export { default as Signin } from './Signin/Signin';
